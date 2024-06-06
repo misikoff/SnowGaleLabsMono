7e947f2024d2c9b98d0114409b1a2f5d2985c538
Thomas Misikoff <tmisikoff@gmail.com>
2024-06-06 08:53:08 -0500

    fix: add issue comment

--- db/index.ts
+++ db/index.ts
@@ -7,6 +7,7 @@ const schema = {
   ...schemas,
 }
 
+// https://github.com/drizzle-team/drizzle-orm/issues/2006
 const sql: NeonQueryFunction<boolean, boolean> = neon(process.env.DATABASE_URL!) // neon(process.env.DRIZZLE_DATABASE_URL!)
 const db = drizzle(sql, { schema })
 
