262c5aa313ef2d9311b361e5e66e2f746d6f38ff
Thomas Misikoff <tmisikoff@gmail.com>
2024-11-01 19:06:20 -0500

    fix: get native building again with pnpm

--- apps/native/metro.config.js
+++ apps/native/metro.config.js
@@ -16,10 +16,9 @@ const workspaceRoot = path.resolve(projectRoot, '../..')
 
 const config = getDefaultConfig(projectRoot)
 
+// Since we are using pnpm, we have to setup the monorepo manually for Metro
 // #1 - Watch all files in the monorepo
 config.watchFolders = [workspaceRoot]
-// #3 - Force resolving nested modules to the folders below
-config.resolver.disableHierarchicalLookup = true
 // #2 - Try resolving with project modules first, then workspace modules
 config.resolver.nodeModulesPaths = [
   path.resolve(projectRoot, 'node_modules'),
