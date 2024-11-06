379966c71fd0ee61396f69745ce68a07f1b10aee
Thomas Misikoff <tmisikoff@gmail.com>
2024-11-05 19:49:39 -0600

    fix: sign up works without email verification

--- apps/native/app/(public)/sign-up.tsx
+++ apps/native/app/(public)/sign-up.tsx
@@ -5,15 +5,13 @@ import { Stack } from 'expo-router'
 import { useSignUp } from '@clerk/clerk-expo'
 import Spinner from 'react-native-loading-spinner-overlay'
 
-import { LogoutButton } from '../(auth)/_layout'
-
 const Register = () => {
   const { isLoaded, signUp, setActive } = useSignUp()
 
   const [emailAddress, setEmailAddress] = useState('')
   const [password, setPassword] = useState('')
-  const [pendingVerification, setPendingVerification] = useState(false)
-  const [code, setCode] = useState('')
+  // const [pendingVerification, setPendingVerification] = useState(false)
+  // const [code, setCode] = useState('')
   const [loading, setLoading] = useState(false)
 
   // Create the user and send the verification email
@@ -25,16 +23,20 @@ const Register = () => {
 
     try {
       // Create the user on Clerk
-      await signUp.create({
+      const signup = await signUp.create({
         emailAddress,
         password,
       })
 
+      // TODO get email verification
       // Send verification Email
-      await signUp.prepareEmailAddressVerification({ strategy: 'email_code' })
+      // await signUp.prepareEmailAddressVerification({ strategy: 'email_code' })
 
       // change the UI to verify the email address
-      setPendingVerification(true)
+      // setPendingVerification(true)
+
+      console.log({ signup })
+      await setActive({ session: signup.createdSessionId })
     } catch (err: any) {
       console.log({ err })
       console.log({ x: err[0] })
@@ -45,54 +47,56 @@ const Register = () => {
   }
 
   // Verify the email address
-  const onPressVerify = async () => {
-    if (!isLoaded) {
-      return
-    }
-    setLoading(true)
+  // const onPressVerify = async () => {
+  //   if (!isLoaded) {
+  //     return
+  //   }
+  //   setLoading(true)
 
-    try {
-      const completeSignUp = await signUp.attemptEmailAddressVerification({
-        code,
-      })
+  //   try {
+  //     const completeSignUp = await signUp.attemptEmailAddressVerification({
+  //       code,
+  //     })
 
-      await setActive({ session: completeSignUp.createdSessionId })
-    } catch (err: any) {
-      alert(err.errors[0].message)
-    } finally {
-      setLoading(false)
-    }
-  }
+  //     await setActive({ session: completeSignUp.createdSessionId })
+  //   } catch (err: any) {
+  //     alert(err.errors[0].message)
+  //   } finally {
+  //     setLoading(false)
+  //   }
+  // }
 
   return (
     <View className='flex-1 content-center p-5'>
-      <Stack.Screen options={{ headerBackVisible: !pendingVerification }} />
+      <Stack.Screen
+      // options={{ headerBackVisible: !pendingVerification }}
+      />
       <Spinner visible={loading} />
 
       <Text>{isLoaded}wowowowo</Text>
 
-      {!pendingVerification && (
-        <>
-          <TextInput
-            autoCapitalize='none'
-            placeholder='example@domain.com'
-            value={emailAddress}
-            onChangeText={setEmailAddress}
-            className='my-4 h-14 rounded-md border-2 border-purple-600 bg-white p-4'
-          />
-          <TextInput
-            placeholder='password'
-            value={password}
-            onChangeText={setPassword}
-            secureTextEntry
-            className='my-4 h-14 rounded-md border-2 border-purple-600 bg-white p-4'
-          />
+      {/* {!pendingVerification && (
+        <> */}
+      <TextInput
+        autoCapitalize='none'
+        placeholder='example@domain.com'
+        value={emailAddress}
+        onChangeText={setEmailAddress}
+        className='my-4 h-14 rounded-md border-2 border-purple-600 bg-white p-4'
+      />
+      <TextInput
+        placeholder='password'
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+        className='my-4 h-14 rounded-md border-2 border-purple-600 bg-white p-4'
+      />
 
-          <Button onPress={onSignUpPress} title='Sign up' color={'#6c47ff'} />
-        </>
-      )}
+      <Button onPress={onSignUpPress} title='Sign up' color={'#6c47ff'} />
+      {/* </>
+      )} */}
 
-      {pendingVerification && (
+      {/* {pendingVerification && (
         <>
           <View>
             <TextInput
@@ -108,10 +112,7 @@ const Register = () => {
             color={'#6c47ff'}
           />
         </>
-      )}
-      {/* debugging */}
-      {/* delete */}
-      <LogoutButton />
+      )} */}
     </View>
   )
 }
