5bd223ca97cc3bb322ddc19a7ae2f99efbb9b94c
Thomas Misikoff <tmisikoff@gmail.com>
2024-12-14 00:50:11 -0600

    feat: get two way scroller working

--- apps/native/app/(auth)/calendar/index.tsx
+++ apps/native/app/(auth)/calendar/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 import {
   Dimensions,
@@ -6,8 +6,31 @@ import {
   View,
   TouchableOpacity,
   ScrollView,
+  Button,
 } from 'react-native'
 import clsx from 'clsx'
+
+// get 9 weeks centered around today
+const getSortedChunks = () => {
+  const today = new Date()
+  const days: string[] = []
+
+  // Collect 31 days before today, today, and 31 days after today
+  for (let i = -31; i <= 31; i++) {
+    const date = new Date(today)
+    date.setDate(today.getDate() + i)
+
+    // Adjust the date to match the local time zone
+    date.setHours(0, 0, 0, 0) // Set the time to midnight to avoid time zone issues
+    days.push(date.toISOString().split('T')[0]) // Format as YYYY-MM-DD
+  }
+
+  // Sort the dates and chunk into weeks of 7 days
+  return Array.from(
+    { length: 9 },
+    (_, i) => days.slice(i * 7, i * 7 + 7), // Slice into 7-day chunks
+  )
+}
 
 function getSurroundingDays() {
   const today = new Date() // Get the current date
@@ -24,13 +47,20 @@ function getSurroundingDays() {
   return surroundingDays
 }
 
+const weeks = getSortedChunks()
+const days = getSurroundingDays()
+
 export default function Calendar() {
+  const scrollViewRef = useRef(null)
+  const subScrollViewRef = useRef(null)
   const DeviceSize = {
-    width: Dimensions.get('window'),
+    width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
   }
-  const [curDay, setCurDay] = useState(4)
-  const days = getSurroundingDays()
+  const [curWeek, setCurWeek] = useState(4)
+  const [curDay, setCurDay] = useState(3)
+  const [selectedDate, setSelectedDate] = useState(weeks[4][3])
+  const [selectedWeek, setSelectedWeek] = useState(weeks[4])
 
   const quotes: { quote: string; author: string }[] = [
     {
@@ -67,8 +97,8 @@ export default function Calendar() {
   ]
 
   // match quotes to days
-  const dates = days.map((day) => {
-    const quote = quotes[day % quotes.length]
+  const dates = selectedWeek.map((day, i) => {
+    const quote = quotes[i % quotes.length]
     return {
       day,
       quote: quote.quote,
@@ -78,42 +108,80 @@ export default function Calendar() {
 
   return (
     <View className='flex-1 items-center'>
-      <Text className='text-2xl font-bold'>
-        {new Date().toLocaleString('en-US', { month: 'short' }) +
-          " '" +
-          new Date().getFullYear().toString().substring(2)}
-      </Text>
+      <View className='w-full flex-row justify-between p-4'>
+        <Text className='text-2xl font-bold'>
+          {new Date(weeks[curWeek][3]).toLocaleString('en-US', {
+            month: 'short',
+          }) +
+            " '" +
+            new Date(weeks[curWeek][3]).getFullYear().toString().substring(2)}
+        </Text>
+        <Button
+          title='Today'
+          onPress={() => {
+            setCurWeek(3)
+            scrollViewRef.current?.scrollTo({
+              x: DeviceSize.width * 4,
+              animated: true,
+            })
+          }}
+        />
+      </View>
       <ScrollView
         horizontal={true}
+        showsHorizontalScrollIndicator={false}
         pagingEnabled={true}
+        ref={scrollViewRef}
+        onMomentumScrollEnd={(event) => {
+          console.log(
+            'event.nativeEvent.contentOffset.x:',
+            event.nativeEvent.contentOffset.x,
+            DeviceSize.width,
+          )
+          const page = event.nativeEvent.contentOffset.x / DeviceSize.width
+          console.log('page:', page)
+          setCurWeek(Math.floor(page))
+        }}
         className='flex-grow-0'
       >
-        <View className='w-screen flex-row justify-between pt-4'>
-          {dates.map((date) => (
-            <TouchableOpacity
-              key={date.day}
-              className={clsx(
-                'flex-grow border-b-2 pb-4',
-                date.day === curDay ? 'border-blue-500' : 'border-gray-400',
-              )}
-              onPress={() => setCurDay(date.day)}
-            >
-              <View className='text-center'>
-                <Text
-                  className={clsx(
-                    'mx-auto text-lg font-bold',
-                    date.day === curDay && 'text-blue-500',
-                  )}
-                >
-                  {date.day}
-                </Text>
-              </View>
-            </TouchableOpacity>
-          ))}
-        </View>
+        {weeks.map((week, i) => (
+          <View key={i} className='w-screen flex-row justify-between pt-4'>
+            {week.map((date, d) => (
+              <TouchableOpacity
+                key={date}
+                className={clsx(
+                  'flex-grow border-b-2 pb-4',
+                  date === selectedDate ? 'border-blue-500' : 'border-gray-400',
+                )}
+                onPress={() => {
+                  setSelectedDate(date)
+                  setSelectedWeek(week)
+                  subScrollViewRef.current?.scrollTo({
+                    x: DeviceSize.width * d,
+                    animated: true,
+                  })
+                  // setCurDay(d)
+                  setCurWeek(i)
+                }}
+              >
+                <View className='text-center'>
+                  <Text
+                    className={clsx(
+                      'mx-auto text-lg font-bold',
+                      date === selectedDate && 'text-blue-500',
+                    )}
+                  >
+                    {date.split('-')[2]}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            ))}
+          </View>
+        ))}
       </ScrollView>
 
       <ScrollView
+        ref={subScrollViewRef}
         className='bg-gray-500'
         horizontal={true}
         pagingEnabled={true}
@@ -123,10 +191,10 @@ export default function Calendar() {
             event.nativeEvent.contentOffset.x,
             DeviceSize.width,
           )
-          const page =
-            event.nativeEvent.contentOffset.x / DeviceSize.width.width
+          const page = event.nativeEvent.contentOffset.x / DeviceSize.width
           console.log('page:', page)
-          setCurDay(days[Math.floor(page)])
+          // setCurDay(days[Math.floor(page)])
+          setSelectedDate(dates[Math.floor(page)].day)
         }}
       >
         {dates.map((date) => (
@@ -135,6 +203,7 @@ export default function Calendar() {
             className='w-screen items-center justify-center bg-gray-300'
           >
             <View className='w-64 gap-2'>
+              <Text className='text-center'>{date.day}</Text>
               <Text className='text-center'>{date.quote}</Text>
               <Text className='text-center font-bold'>{date.author}</Text>
             </View>
