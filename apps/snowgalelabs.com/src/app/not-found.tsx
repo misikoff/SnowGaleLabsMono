d811e2a18d5e7a9b60690a46d780ab00f6c6751d
Thomas Misikoff <tmisikoff@gmail.com>
2025-02-18 20:16:18 -0600

    fix: 404

--- apps/snowgalelabs.com/src/app/not-found.tsx
+++ apps/snowgalelabs.com/src/app/not-found.tsx
@@ -1,12 +1,13 @@
 import Link from 'next/link'
 
 import { Container } from '@/components/Container'
-import { FadeIn } from '@/components/FadeIn'
+// import { FadeIn } from '@/components/FadeIn'
 
 export default function NotFound() {
   return (
     <Container className="flex h-full items-center pt-24 sm:pt-32 lg:pt-40">
-      <FadeIn className="flex max-w-xl flex-col items-center text-center">
+      <div className="flex max-w-xl flex-col items-center text-center">
+      {/* <FadeIn className="flex max-w-xl flex-col items-center text-center"> */}
         <p className="font-display text-4xl font-semibold text-neutral-950 sm:text-5xl">
           404
         </p>
@@ -22,7 +23,8 @@ export default function NotFound() {
         >
           Go to the home page
         </Link>
-      </FadeIn>
+      {/* </FadeIn> */}
+      </div>
     </Container>
   )
 }
