75f6d3916ea2c2cf9ba87e6389743475636075eb
Thomas Misikoff <tmisikoff@gmail.com>
2025-02-18 21:15:48 -0600

    feat: try fancy site

---
+++ apps/snowgalelabs/src/components/FadeIn.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import { createContext, useContext } from 'react'
+import { motion, useReducedMotion } from 'framer-motion'
+
+const FadeInStaggerContext = createContext(false)
+
+const viewport = { once: true, margin: '0px 0px -200px' }
+
+export function FadeIn(
+  props: React.ComponentPropsWithoutRef<typeof motion.div> & {className?: string}
+) {
+  const shouldReduceMotion = useReducedMotion()
+  const isInStaggerGroup = useContext(FadeInStaggerContext)
+
+  return (
+    <motion.div
+      className={props.className || ''}
+      variants={{
+        hidden: { opacity: 0, y: shouldReduceMotion ? 0 : 24 },
+        visible: { opacity: 1, y: 0 },
+      }}
+      transition={{ duration: 0.5 }}
+      {...(isInStaggerGroup
+        ? {}
+        : {
+            initial: 'hidden',
+            whileInView: 'visible',
+            viewport,
+          })}
+      {...props}
+    />
+  )
+}
+
+export function FadeInStagger({
+  faster = false,
+  ...props
+}: React.ComponentPropsWithoutRef<typeof motion.div> & { faster?: boolean, className?: string }) {
+  return (
+    <FadeInStaggerContext.Provider value={true}>
+      <motion.div
+      className={props.className || ''}
+        initial="hidden"
+        whileInView="visible"
+        viewport={viewport}
+        transition={{ staggerChildren: faster ? 0.12 : 0.2 }}
+        {...props}
+      />
+    </FadeInStaggerContext.Provider>
+  )
+}
