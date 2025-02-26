4e9879cf61466720b7765e9c56e16a529dec9f30
Thomas Misikoff <tmisikoff@gmail.com>
2025-02-25 18:20:43 -0600

    feat: set up eas

--- apps/toron-native/app/(auth)/index.tsx
+++ apps/toron-native/app/(auth)/index.tsx
@@ -9,7 +9,7 @@ export default function Tab() {
   return (
     <View className='flex-1 items-center justify-center gap-y-8'>
       <Text className='text-center text-xl font-extrabold'>
-        {user && `Welcome to Centurion,\n${user.data.user?.email}`}
+        {user && `Welcome to Toron,\n${user.data.user?.email}`}
       </Text>
       <Link href='/session'>
         <View className='rounded-md bg-blue-600 px-3 py-2 text-center'>
