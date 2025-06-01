5354e8042c9d80e85d0084942e5078af72a01935
Thomas Misikoff <tmisikoff@gmail.com>
2025-05-31 19:45:14 -0500

    fix: try again

--- apps/toron-web/src/app/api/deleteUser/route.ts
+++ apps/toron-web/src/app/api/deleteUser/route.ts
@@ -3,8 +3,8 @@ import { createClient } from '@supabase/supabase-js'
 import jwt from 'jsonwebtoken'
 
 const supabaseAdmin = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL || '',
-  process.env.SUPABASE_SERVICE_ROLE_KEY || '',
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!,
 )
 
 export async function DELETE() {
